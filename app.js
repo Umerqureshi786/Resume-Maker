c9cbd85dd2ca67a1c862a6d09373948a2f6fed42
Umar Qureshi <161929656+Umerqureshi786@users.noreply.github.com>
2024-11-08 16:46:26 +0500

    Static Resume

---
+++ app.js
@@ -0,0 +1,24 @@
+// var a=5;
+// console.log(a);
+
+// print("Hello world");
+
+console.log("hello woeld");
+
+//Variables in Js.......
+let myName = "Umar"; //String
+let age = 26;  //Number
+let isStudent = true;  //Bolean
+// let a; //undefined
+let b = {};
+
+let myArray = ["Ali", 1, 2];  //Array
+
+let myobj = {  //Object
+    name: "umar",
+    gender: "male",
+    contact: 923482500818
+};
+
+console.log(myName, age, isStudent, b, myArray, myobj);
+alert("Hello world");
