38b1c5aaeb79e5c92a56179acef1b765d7396674
Umar Qureshi <161929656+Umerqureshi786@users.noreply.github.com>
2024-11-14 13:19:55 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-// var a=5;
-// console.log(a);
 
-// print("Hello world");
+function getformdata() {
+    const yourName = document.getElementById("fname").value;
+    const yourEmail = document.getElementById("email").value;
+    const dateOfBirth = document.getElementById("Dob").value;
+    const contactNum = document.getElementById("Contact").value;
 
-console.log("hello woeld");
-
-//Variables in Js.......
-let myName = "Umar"; //String
-let age = 26;  //Number
-let isStudent = true;  //Bolean
-// let a; //undefined
-let b = {};
-
-let myArray = ["Ali", 1, 2];  //Array
-
-let myobj = {  //Object
-    name: "umar",
-    gender: "male",
-    contact: 923482500818
+    console.log(yourName, yourEmail, dateOfBirth, contactNum);
 };
 
-console.log(myName, age, isStudent, b, myArray, myobj);
-alert("Hello world");
+
+function createResume() {
+    const degreeName = document.getElementById("Degreeoptions").value;
+    const colUniName=document.getElementById("select").value;
+    const gradeObtain = document.getElementById("gradeobtain").value;
+    const skillsHave = document.getElementById("skilldetails").value;
+    const workExp = document.getElementById("experience").value;
+
+    console.log(degreeName,colUniName,gradeObtain,skillsHave,workExp);
+
+}
+
+
+
